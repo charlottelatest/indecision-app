af4407b1d5d1b24fcc3a8d9d92dc65865d55b54d
Charlotte Zhang <jsdryan@gmail.com>
2022-03-05 16:38:48 +0800

    Finish 46: Saving and Loading the Count.

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,19 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count,
+      count: 0,
     };
+  }
+  componentDidMount() {
+    const count = parseInt(localStorage.getItem('count'));
+    if (!isNaN(count)) {
+      this.setState(() => ({ count }));
+    }
+  }
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
   }
   handlePlusOne() {
     this.setState((prevState) => {
@@ -41,63 +52,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 0,
-};
-
-ReactDOM.render(<Counter count={0} />, document.querySelector('#main'));
-
-// const appProp = {
-//   title: 'Indecision App',
-//   subtitle: 'Put your life in the hands of a computer',
-//   options: ['A', 'B', 'C'],
-// };
-
-// const getOptions = (options) => {
-//   return options && options.length > 0 ? (
-//     <p>Here are your options: {options.join(',')}</p>
-//   ) : (
-//     'No options'
-//   );
-// };
-
-// const template = (
-//   <div>
-//     <h1>{appProp.title ? appProp.title : 'Untitled App'}</h1>
-//     {appProp.subtitle && <p>{appProp.subtitle}</p>}
-//     {getOptions(appProp.options)}
-//   </div>
-// );
-
-// let count = 0;
-
-// const handleAddOne = () => {
-//   console.log('add one');
-//   count++;
-//   renderCounterApp();
-// };
-
-// const handleMinusOne = () => {
-//   console.log('minus one');
-//   count--;
-//   renderCounterApp();
-// };
-
-// const handleReset = () => {
-//   console.log('reset');
-// };
-
-// const renderCounterApp = () => {
-//   const template = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={handleAddOne}>+1</button>
-//       <button onClick={handleMinusOne}>-1</button>
-//       <button onClick={handleReset}>Reset</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(template, document.querySelector('#main'));
-// };
-
-// renderCounterApp();
+ReactDOM.render(<Counter />, document.querySelector('#main'));
