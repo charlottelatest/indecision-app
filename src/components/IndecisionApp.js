f3e5fdac63aa0ca4b1765050074a45ee8287dbe8
Charlotte Zhang <jsdryan@gmail.com>
2022-03-06 15:50:09 +0800

    Finish 58: ES6 class properties.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,32 +5,9 @@ import Header from './Header';
 import Action from './Action';
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleRemoveOne = this.handleRemoveOne.bind(this);
-    this.state = {
-      options: [],
-    };
-  }
-  componentDidMount() {
-    try {
-      const json = localStorage.getItem('options');
-      const options = JSON.parse(json);
-      if (options) {
-        this.setState(() => ({ options }));
-      }
-    } catch (e) {}
-  }
-  componentDidUpdate(_prevProps, prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json);
-    }
-  }
-  handleAddOption(option) {
+  state = { options: [] };
+
+  handleAddOption = (option) => {
     if (!option) {
       return 'Please enter a valid option.';
     } else if (this.state.options.indexOf(option) > -1) {
@@ -40,20 +17,41 @@ export default class IndecisionApp extends React.Component {
     this.setState((prevState) => ({
       options: prevState.options.concat([option]),
     }));
-  }
-  handlePick() {
+  };
+
+  handlePick = () => {
     const options = this.state.options;
     const randNum = Math.floor(Math.random() * options.length);
     alert(options[randNum]);
-  }
-  handleRemoveAll() {
+  };
+
+  handleRemoveAll = () => {
     this.setState(() => ({ options: [] }));
-  }
-  handleRemoveOne(option) {
+  };
+
+  handleRemoveOne = (option) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((o) => option !== o),
     }));
+  };
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {}
   }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
   render() {
     const subtitle = 'Put your life in the hands of a computer';
 
